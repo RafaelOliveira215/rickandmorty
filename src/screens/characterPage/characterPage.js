06847b27f98f54df017148841a9e09c2f9d41046
RafaelOliveira215 <rafaelllmoliveira@gmail.com>
2021-04-23 21:22:31 -0300

    requisição de personagens da primeira pagina e mapeamento sem estilização

--- src/screens/characterPage/characterPage.js
+++ src/screens/characterPage/characterPage.js
@@ -1,12 +1,29 @@
-import React from "react"
+import axios from "axios"
+import React, {useState,useEffect} from "react"
 
 const CharacterPage = () =>{
+    const [characters,setCharacters] = useState()
+    
+    useEffect(()=>{
+        axios.get("https://rickandmortyapi.com/api/character").then(response=>{
+            setCharacters(response)
+        })
+    },[])
+    console.log(characters)
+   
 
     return(
         <div>
-            funciona character page
+         {characters.data.results.map((character)=>{
+             return(
+                 <div>
+                     {character.name}
+                     <img src={character.image}/>
+                     {character.status}
+                 </div>
+             )
+         })}
         </div>
     )
 }
-
 export default CharacterPage
